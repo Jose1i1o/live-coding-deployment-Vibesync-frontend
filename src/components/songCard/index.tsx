4eb8ac9c2b291fb97269a72e2d0292552f01e496
Jose Valenzuela <guadix88@gmail.com>
2024-05-03 14:00:53 +0200

    fix(build target): added build target to es2022

--- src/components/songCard/index.tsx
+++ src/components/songCard/index.tsx
@@ -1,62 +1,74 @@
-import { type Track } from '../../types/data'
-import './SongCard.css'
-import { useAudioContext } from '../../hooks/useAudio'
-import { useQuery } from '@tanstack/react-query'
-import { useEffect } from 'react'
-import { TracksService } from '../../services/TracksService'
+import { type Track } from "../../types/data";
+import "./SongCard.css";
+import { useAudioContext } from "../../hooks/useAudio";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { TracksService } from "../../services/TracksService";
 
 interface Props {
-  track: Track
-  isActive?: boolean
+	track: Track;
+	isActive?: boolean;
 }
 
 const SongCard = ({ track }: Props) => {
-  const { setAudioUrl, setAudioImg, trackId, setTrackId, setIsPlaying, getSongDuration, audioRef, setSongDuration } = useAudioContext()
+	const {
+		setAudioUrl,
+		setAudioImg,
+		trackId,
+		setTrackId,
+		setIsPlaying,
+		getSongDuration,
+		audioRef,
+		setSongDuration,
+	} = useAudioContext();
 
-  const getTrack = async (trackId: string | undefined) => {
-    if (trackId) {
-      const track = await TracksService.getTrack(trackId)
-      getSongDuration(audioRef, setSongDuration)
-      setAudioUrl(track.url)
-      setAudioImg(track.thumbnail)
-      localStorage.setItem('localTrack', JSON.stringify(track))
-      setTimeout(() => {
-        setTrackId(0)
-        void queryTrack.refetch()
-      }, 90)
-      return track
-    } else {
-      const track = JSON.parse(localStorage.getItem('localTrack') || '{}')
-      setAudioUrl(track.url)
-      setAudioImg(track.thumbnail)
-      return track
-    }
-  }
-  const queryTrack = useQuery({
-    queryKey: ['track'],
-    queryFn: async () => await getTrack(trackId)
-  })
+	const getTrack = async (trackId: string | undefined) => {
+		if (trackId) {
+			const track = await TracksService.getTrack(trackId);
+			getSongDuration(audioRef, setSongDuration);
+			setAudioUrl(track.url);
+			setAudioImg(track.thumbnail);
+			localStorage.setItem("localTrack", JSON.stringify(track));
+			setTimeout(() => {
+				setTrackId(0);
+				void queryTrack.refetch();
+			}, 90);
+			return track;
+		} else {
+			const track = JSON.parse(localStorage.getItem("localTrack") || "{}");
+			setAudioUrl(track.url);
+			setAudioImg(track.thumbnail);
+			return track;
+		}
+	};
+	const queryTrack = useQuery({
+		queryKey: ["track"],
+		queryFn: async () => await getTrack(trackId),
+	});
 
-  useEffect(() => {
-    const track = JSON.parse(localStorage.getItem('localTrack') || '{}')
-    setTrackId(track.id)
-  }, [])
+	useEffect(() => {
+		const track = JSON.parse(localStorage.getItem("localTrack") || "{}");
+		setTrackId(track.id);
+	}, []);
 
-  return (
-    <div className="songcard-container" onClick={() => {
-      setTrackId(track.id)
-      setIsPlaying(false)
-      setTimeout(() => {
-        void queryTrack.refetch()
-      }, 90)
-    }}>
-      <img className="songcard-img" src={track.thumbnail} />
-      <div className="songcard-track-info">
-        <h3>{track.name}</h3>
-        <p>{track.artist}</p>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div
+			className="songcard-container"
+			onClick={() => {
+				setTrackId(track.id);
+				setIsPlaying(false);
+				setTimeout(() => {
+					void queryTrack.refetch();
+				}, 90);
+			}}
+		>
+			<img className="songcard-img" src={track.thumbnail} />
+			<div className="songcard-track-info">
+				<h3>{track.name}</h3>
+				<p>{track.artist}</p>
+			</div>
+		</div>
+	);
+};
 
-export default SongCard
+export default SongCard;
